90b4e41b1f22abbc2541e137e83059cfd6c7f595
John Leacox <johnlcox@users.noreply.github.com>
2014-04-26 15:53:32 -0500

    Update mac jdk 7 url to 7u55

--- attributes/default_mac_os_x.rb
+++ attributes/default_mac_os_x.rb
@@ -3,4 +3,4 @@ default['java']['jdk_versions'] = ['7']
 
 default['java']['jdk']['6']['mac_os_x']['url'] = "http://support.apple.com/downloads/DL1572/en_US/JavaForOSX2013-05.dmg"
 
-default['java']['jdk']['7']['mac_os_x']['url'] = "http://download.oracle.com/otn-pub/java/jdk/7u45-b18/jdk-7u45-macosx-x64.dmg"
+default['java']['jdk']['7']['mac_os_x']['url'] = "http://download.oracle.com/otn-pub/java/jdk/7u55-b13/jdk-7u55-macosx-x64.dmg"
